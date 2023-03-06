e0c39dbd0565749dc496f0a459c69796881b18d9
RobertoSimonini <robertosimonini1@gmail.com>
2023-03-06 15:32:18 +0100

    index crud done

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -12,7 +12,9 @@ class ExamController extends Controller
      */
     public function index()
     {
-        //
+        $exams = Exam::all();
+
+        return view('exams.index', compact('exams'));
     }
 
     /**
