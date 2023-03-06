43aaee65f8580330484fca89e5106fd6ff707bd6
Ali-Chouchene <choucheneali9801@gmail.com>
2023-03-06 15:44:01 +0100

    header fixed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.main')
 @section('content')
 <div id="home" class="container mt-3">
-    <div class="d-flex justify-content-center">
+    <div class="d-flex justify-content-center rounded">
         <img src="https://cdn.wallpapersafari.com/25/64/6jVwBz.jpg" alt="">
         <div id="text" class="w-50">
             <h1>Welcome in our Medical Center, on our website you can find the results of youre exams.</h1>
