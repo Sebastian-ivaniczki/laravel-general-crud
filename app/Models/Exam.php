7ebdf43480baf866ac634af18bd610ce696c4441
Sebastian-ivaniczki <sivaniczki@gmail.com>
2023-03-06 14:13:38 +0100

    added fillable ad exam model

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Exam extends Model
 {
     use HasFactory;
+    protected $fillable = ['surname', 'name', 'fiscal_code', 'date_of_birth', 'genre', 'exam_type', 'blood_platelet', 'white_blood_cells', 'hemoglobin', 'hematocrit', 'average_cell_volume', 'average_cell_content_hb', 'average_cell_concentration', 'gr', 'mean_plate_volume', 'hypochromic_red_blood_cells', 'sgpt'];
 }
